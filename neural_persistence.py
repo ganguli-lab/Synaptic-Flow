76a488ac07f226e9640a356a00d082c1ced25c6d
Aishwarya Balwani <aishwaryabalwani@gmail.com>
2022-01-11 18:04:37 -0500

    With conv NP
    Code needs checking

--- neural_persistence.py
+++ neural_persistence.py
@@ -2,26 +2,32 @@ import numpy as np
 from numpy.linalg import norm
 import networkx as nx
 import torch
+from torch.linalg import norm as torchnorm
 
 def neural_persistence(model):
-    total = {}
+    totalNP = {}
+    sortedEdges = {}
+
     def compute_NP(name):
+
         def hook(module, input, output):
-            NPs = {}
-            if isinstance(module, layers.Linear) or isinstance(module, nn.Linear):
+            # NPs = {}
+
+            if isinstance(module, layers.Linear) or isinstance(module, nn.Linear): ## Fully connected layers
                 """
                 Uses networkx to compute the MST
                 """
-                # extract weights
-                weights = module.weights
+                ## extract weights
+                ## shape is (nodesIn, nodesOut)
+                weights_np = module.weights.detach().cpu().numpy()
 
-                # find the largest weight
-                h_max = torch.max(weights)
+                ## find the largest weight
+                h_max = np.max(np.abs(weights_np))
 
-                # normalize by the largest weight
-                W_prime = torch.abs(weights/h_max)
+                ## normalize by the largest weight
+                W_prime = np.abs(weights_np/h_max)
 
-                d1, d2 = weights.shape
+                d1, d2 = weights_np.shape
 
                 cardV = np.sum(d1+d2)
                 cardMST = cardV - 1
@@ -34,9 +40,6 @@ def neural_persistence(model):
 
                 Adj += Adj.T
 
-                cardV, cardV = Adj.shape
-                cardMST = cardV - 1
-
                 Gr = nx.from_numpy_array(Adj)
                 Tr = nx.maximum_spanning_tree(Gr)
 
@@ -46,80 +49,106 @@ def neural_persistence(model):
                 for kk in range(cardMST):
                     d_arr[kk] = sorted(Tr.edges(data=True))[kk][2]['weight']
 
-                pdMat = np.hstack((c_arr,d_arr))
+                pdMat = torch.hstack((c_arr,d_arr))
 
-                pers = np.abs(np.diff(pdMat,axis=-1))
+                pers = torch.abs(np.diff(pdMat,axis=-1))
                 layerNP = norm(pers,ord=2)
                 if normalized:
                     layerNP = (layerNP-0)/((cardV-2)**0.5)
 
-            return layerNP, sorted(Tr.edges(data=True))
+                totalNP[name] = layerNP
+                sortedEdges[name] = sorted(Tr.edges(data=True))
+            # return layerNP, sorted(Tr.edges(data=True))
 
+            if isinstance(module, layers.Conv2d) or isinstance(module, nn.Conv2d): ## Convolutional Layers
 
-            if isinstance(module, layers.Conv2d) or isinstance(module, nn.Conv2d): #computes on a per filter basis
-                """
-                Pseudocode was obtained from Section A.4 in Rieck et al., 2019
+                ## extract weights
+                ## weights shape = (#filters, #channels, krows, kcols)
+                weights_np = module.weights.detach().cpu().numpy()
 
-                """
-                # extract weights
-                weights = module.weights
+                nFilters = weights_np.shape[0]
+                allWeightsSum = np.sum(np.abs(weights_np),axis=1) ## Absolute weights summed across channels of a filter
 
-                # initalize number of tuples, tuple counter, weight index
-                # number of input neurons (m), number of output neurons (n)
-                m = module.in_features
-                n = module.out_features
-                tau = m + n
-                t = 0
-                i = 0
+                filterNP_list = np.zeros(nFilters)
+                filterEdges = {}
 
-                # determine largest absolute weight
-                h_max = torch.max(weights)
+                for filtNum in range(nFilters):
+                    h_max = np.max(allWeightsSum[filtNum])
+                    W_prime = allWeightsSum[filtNum]/h_max ## Normalized weights of conv kernel
 
-                # transform weights for filtration
-                h_prime = torch.abs(weights/h_max)
-
-                # sort weights in descending order
-                h_sort =  torch.sort(torch.flatten(h_prime), dim=0, descending=True)
-
-                # determine the set of all corner weights for filter H', p & q are filter dimensions
-                p = module.conv.kernel_size[0]
-                q = module.conv.kernel_size[1]
-
-                h_corner = {h_prime[0,0],h_prime[0,q-1], h_prime[p-1,0], h_prime[p-1,q-1]}
-                # get the indices for the corner weights in the vectorized version of the filter
-                corner_idx = [0, q-1, p*(q-1), (p*q)-1]
-
-                # add tuple for surviving component
-                NPs.append((1,0))
-
-                # Each corner of H' merges components
-                for c in range(len(h_corner)):
-                    NPs.append((1, h_corner[c])
-                    t+=1
-
-                # create the remaining tuples
-                while 1 do:
-                    # if current weight is corner, write one less tuple
-                    if i in corner_idx:
-                        n_prime = n - 1
+                    if len(W_prime.shape)==3:
+                        nChannels, kRows, kCols = W_prime.shape ##W_prime is a 3D tensor
                     else:
-                        n_prime = n
+                        nChannels = 1
+                        kRows, kCols = W_prime.shape ##W_prime is a 2D tensor, same size as spatial kernel
 
-                    # if there are at least n' ,or tuples, set merge value to sorted[i]
-                    if t+n_prime <= tau:
-                        for j in range(n_prime):
-                            NPs.append((1,h_sort[i]))
-                        t+=n_prime
-                        i+=1
-                    else:
-                        for j in range(tau-t):
-                            NPs.append((1, h_sort[i]))
-                        break
-                # Compute norm of approximated persistence diagram
-                NPs = torch.norm(NPs, 2)
+                    inputSizes, outputSizes = in_out_sizes
 
-            total[name] = NPs
+                    inRows, inCols = inputSizes
+                    outRows, outCols = outputSizes
+
+                    nodesIn = np.prod(inputSizes)
+                    nodesOut = np.prod(outputSizes)
+
+                    cardV = np.sum(nodesIn+nodesOut)
+                    cardMST = cardV - 1
+
+                    bigW_prime = np.zeros((nodesOut,nodesIn))
+
+                    ##Arrange conv weights into FC-type weight matrix bigW_prime
+                    convWeights = np.zeros(nodesIn)
+                    for ii in range(kRows):
+                        convWeights[(ii*inCols):(ii*inCols)+kCols] = W_prime[ii]
+
+                    ## assume stride of 1 in both directions
+                    nHSteps = inCols - kCols + 1
+                    nVSteps = inRows - kRows + 1
+
+                    rowCtr = 0
+                    colCtr = 0
+
+                    for jj in range(nodesOut):
+                        rollIdx = np.ravel_multi_index(np.array([rowCtr,colCtr]),(inRows,inCols))
+                        bigW_prime[jj] = np.roll(convWeights.copy(),rollIdx)
+                        colCtr += 1
+                        if colCtr%nHSteps == 0:
+                            rowCtr += 1
+                            colCtr = 0
+
+                    Adj = np.zeros((cardV,cardV))
+                    d1, d2 = bigW_prime.shape
+
+                    for ii in range(d1):
+                        for jj in range(d2):
+                            Adj[d1+jj,ii] = bigW_prime[ii,jj]
+
+                    Adj += Adj.T
+
+                    Gr = nx.from_numpy_array(Adj)
+                    Tr = nx.maximum_spanning_tree(Gr)
+
+                    d_arr = np.zeros(cardMST).reshape(-1,1)
+                    c_arr = np.ones(cardMST).reshape(-1,1)
+
+                    for kk in range(cardMST):
+                        d_arr[kk] = sorted(Tr.edges(data=True))[kk][2]['weight']
+
+                    pdMat = torch.hstack((c_arr,d_arr))
+
+                    pers = torch.abs(np.diff(pdMat,axis=-1))
+                    filterNP = norm(pers,ord=2)
+                    if normalized:
+                        filterNP = (filterNP-0)/((cardV-2)**0.5)
+
+                    filterEdges[filtNum] = sorted(Tr.edges(data=True))
+
+                totalNP[name] = np.sum(filterNP_list)
+                sortedEdges[name] = filterEdges
+            # return layerNP, sorted(Tr.edges(data=True))
+
         return hook
+
     for name, module in model.named_modules():
         module.register_forward_hook(compute_NP(name))
-    return total
+
+    return totalNP, sortedEdges
